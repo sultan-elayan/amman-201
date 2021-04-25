7c77cbd0d0b7779a818fd8a8e37af2ba67b44684
sultan-elayan <sultan.m.elayan@gmail.com>
2021-04-25 15:13:25 +0300

    adding lab01b

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let userName = prompt('Tell Me Your Name');
+console.log(userName);
+
+let age = prompt('How Old Are You ?');
+console.log(age);
+
+let city = prompt('Where You Live ?');
+console.log(city);
+
+let Qualification = prompt('What Is Your University Qualification?');
+console.log(Qualification);
+
+
+
+alert(' Hello ' + userName + ' your old is ' + age + ' and your live in ' + city + ' and your University Qualification is ' + Qualification );
